4ed3e07807df08992fb2bceba0e6bf3dd114eed8
Claudio Augusto de Souza Chagas <88558044+ClaudioChagas@users.noreply.github.com>
2022-01-31 21:06:34 -0300

    Add files via upload

---
+++ for.c
@@ -0,0 +1,36 @@
+// CLAUDIO CHAGAS 2022
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define L 5
+#define C 4
+
+int
+main ()
+{
+    int a[L][C];
+    int i;
+    int j;
+
+    for(i=0; i < L; i++)
+    {
+        for(j=0; j < C; j++)
+        {
+            a[i][j] = i;
+        }
+    }
+
+    // apresenta o arraw
+    for(i=0; i < L; i++)
+    {
+        for(j=0; j < C; j++)
+        {
+            printf("%2d \n", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
+    
+}
